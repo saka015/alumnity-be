8f7d8f607a97a6684b91fb7ffc35d6164109a53a
Kamal Nayan <sknasylum8454@gmail.com>
2025-04-07 01:09:14 +0530

    feat: docker setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { JobsModule } from './modules/jobs/jobs.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, JobsModule, AdminModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
